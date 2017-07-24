151614cc128d98726f5df44a1befad3d37a4d431
Luke Lan <luke84320136@gmail.com>
2017-07-24 17:45:44 +0800

    Vue router app
    StarBase Router

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+<template >
+  <div id ="app">
+    <h3>Starbase Router</h3>
+    <h4>
+      <router-link class="link" to="/data/people">來看看角色吧！</router-link> |
+      <router-link class="link" to="/data/planets">探索星球吧！</router-link>
+    </h4>
+    <router-view></router-view>
+  </div>
+</template>
